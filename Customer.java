66ce665b9af2fcf4aac5bf5ba5178a08eea71851
FelipeMockdans <lfmockdans@minha.fag.edu.br>
2025-11-25 19:36:34 -0300

    Commit3

--- Customer.java
+++ Customer.java
@@ -28,7 +28,6 @@ public class Customer {
 
          double thisAmount = amountFor(each);
 
-         // add frequent renter points
          frequentRenterPoints++;
          if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
              each.getDaysRented() > 1)
@@ -45,22 +44,22 @@ public class Customer {
       return result;
    }
 
-   private double amountFor(Rental each) {
+   private double amountFor(Rental aRental) {
       double thisAmount = 0;
 
-      switch (each.getMovie().getPriceCode()) {
+      switch (aRental.getMovie().getPriceCode()) {
          case Movie.REGULAR:
             thisAmount += 2;
-            if (each.getDaysRented() > 2)
-               thisAmount += (each.getDaysRented() - 2) * 1.5;
+            if (aRental.getDaysRented() > 2)
+               thisAmount += (aRental.getDaysRented() - 2) * 1.5;
             break;
          case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
+            thisAmount += aRental.getDaysRented() * 3;
             break;
          case Movie.CHILDRENS:
             thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-               thisAmount += (each.getDaysRented() - 3) * 1.5;
+            if (aRental.getDaysRented() > 3)
+               thisAmount += (aRental.getDaysRented() - 3) * 1.5;
             break;
       }
       return thisAmount;
