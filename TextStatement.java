bdc60544eb32bd5dfb4fd4fc1f83319ef932f6fd
FelipeMockdans <lfmockdans@minha.fag.edu.br>
2025-11-25 20:03:55 -0300

    Commit16

---
+++ TextStatement.java
@@ -0,0 +1,23 @@
+import java.util.Enumeration;
+
+public class TextStatement extends Statement {
+
+   public String value(Customer aCustomer) {
+      Enumeration rentals = aCustomer.getRentals();
+      String result = "Rental Record for " + aCustomer.getName() + "\n";
+
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+
+         result += "\t" + each.getMovie().getTitle() + "\t" +
+                   each.getCharge() + "\n";
+      }
+
+      result += "Amount owed is " + aCustomer.getTotalCharge() + "\n";
+      result += "You earned " +
+                aCustomer.getTotalFrequentRenterPoints() +
+                " frequent renter points";
+
+      return result;
+   }
+}
