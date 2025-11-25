00a7fb2e52012bdc3abc5e581f6ee85e9f7a8f56
FelipeMockdans <lfmockdans@minha.fag.edu.br>
2025-11-25 19:39:31 -0300

    Commit5

--- Customer.java
+++ Customer.java
@@ -5,15 +5,15 @@ public class Customer {
    private String _name;
    private Vector _rentals = new Vector();
 
-   public Customer (String name){
+   public Customer(String name) {
       _name = name;
    }
 
    public void addRental(Rental arg) {
       _rentals.addElement(arg);
    }
-   
-   public String getName (){
+
+   public String getName() {
       return _name;
    }
 
@@ -26,23 +26,23 @@ public class Customer {
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
 
-         double thisAmount = each.getCharge();
-
-         // frequent renter points
+         // add frequent renter points
          frequentRenterPoints++;
          if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
              each.getDaysRented() > 1)
              frequentRenterPoints++;
 
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-                  thisAmount + "\n";
-         totalAmount += thisAmount;
+         // show figures for this rental
+         result += "\t" + each.getMovie().getTitle() + "\t" +
+                   each.getCharge() + "\n";
+
+         totalAmount += each.getCharge();
       }
 
+      // add footer lines
       result += "Amount owed is " + totalAmount + "\n";
       result += "You earned " + frequentRenterPoints +
                 " frequent renter points";
       return result;
    }
 }
-
