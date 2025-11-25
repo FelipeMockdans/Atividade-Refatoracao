4b8eb1d777c280065dfc8f23be93912130e3e340
FelipeMockdans <lfmockdans@minha.fag.edu.br>
2025-11-25 19:53:46 -0300

    Commit11

--- Movie.java
+++ Movie.java
@@ -5,26 +5,38 @@ public class Movie {
   public static final int NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
+  public Movie(String name, int priceCode) {
+      _title = name;
+      setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
+      return _price.getPriceCode();
   }
 
   public void setPriceCode(int arg) {
-      _priceCode = arg;
-  }
+      switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
+   }
 
-  public String getTitle() {
+   public String getTitle() {
       return _title;
-  }
+   }
 
-  public double getCharge(int daysRented) {
+   public double getCharge(int daysRented) {
       double result = 0;
 
       switch (getPriceCode()) {
@@ -45,13 +57,12 @@ public class Movie {
             break;
       }
       return result;
-  }
+   }
 
-  // ⭐ NOVO: método movido de Rental
-  public int getFrequentRenterPoints(int daysRented) {
+   public int getFrequentRenterPoints(int daysRented) {
       if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
          return 2;
       else
          return 1;
-  }
+   }
 }
